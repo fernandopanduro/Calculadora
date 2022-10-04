a98f6cf48f742729ef6732b9a59e012c71be7c26
Fernando Panduro <fernandogp2003pro@gmail.com>
2022-10-04 13:55:44 -0600

    Background con animacion terminado

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      <div className='bubbles'>
+        <span className='bubble-1'></span>
+        <span className='bubble-2'></span>
+        <span className='bubble-3'></span>
+        <span className='bubble-4'></span>
+        <span className='bubble-5'></span>
+        <span className='bubble-6'></span>
+        <span className='bubble-7'></span>
+      </div>
         
     </div>
   );
